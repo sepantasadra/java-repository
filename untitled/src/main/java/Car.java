aa105a4b61d13b5ca03d2fc71bac80c14b2e45c3
Sepantasadra <sepantasadra@gmail.com>
2026-07-08 12:57:29 +0330

    Car class has been added

--- untitled/src/main/java/Car.java
+++ untitled/src/main/java/Car.java
@@ -1,4 +1,6 @@
-package PACKAGE_NAME;
-
 public class Car {
+
+    Car pride = new Car();
+
+
 }
--- untitled/src/test/java/CarTest.java
+++ untitled/src/test/java/CarTest.java
@@ -1,2 +1,45 @@
+import static java.lang.System.*;
+
 public class CarTest {
+
+    class Car {
+
+        boolean carIsOn;
+        boolean doorIsClose;
+        String Model;
+
+        void On() {
+            out.println("Car is on");
+            carIsOn = true;
+        }
+
+        void Off() {
+            out.println("Car is off");
+            carIsOn = false;
+        }
+
+        void toOpenDoor() {
+            out.println("Door is open");
+            doorIsClose = false;
+        }
+
+        void toCloseDoor() {
+            out.println("Door is close");
+            doorIsClose = true;
+        }
+
+        void toDrive() {
+            if (doorIsClose && carIsOn){
+                out.println("We are on the road!");
+            } else if (!doorIsClose && !carIsOn) {
+                out.println("Please close the door and turn on the car");
+            } else if (!doorIsClose) {
+                out.println("Please close the door");
+            } else {
+                out.println("Please turn on the car");
+            }
+        }
+
+
+    }
 }
